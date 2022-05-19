a115014b2a7b69ddd71273af285a33dca76c6e56
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-05-19 10:05:55 -0300

    [IMP] account_invoice_margin: move each model to its own py file

--- account_invoice_margin/models/account_move_line.py
+++ account_invoice_margin/models/account_move_line.py
@@ -4,55 +4,6 @@
 from odoo import api, fields, models
 
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    margin = fields.Monetary(
-        string="Margin",
-        compute="_compute_margin",
-        store=True,
-        currency_field="currency_id",
-    )
-
-    margin_signed = fields.Monetary(
-        string="Margin Signed",
-        compute="_compute_margin",
-        store=True,
-        currency_field="currency_id",
-    )
-
-    margin_percent = fields.Float(
-        string="Margin (%)",
-        digits="Product Price",
-        compute="_compute_margin",
-        store=True,
-    )
-
-    def _get_margin_applicable_lines(self):
-        self.ensure_one()
-        return self.invoice_line_ids
-
-    @api.depends(
-        "invoice_line_ids.margin",
-        "invoice_line_ids.margin_signed",
-        "invoice_line_ids.price_subtotal",
-    )
-    def _compute_margin(self):
-        for invoice in self:
-            margin = 0.0
-            margin_signed = 0.0
-            price_subtotal = 0.0
-            for line in invoice._get_margin_applicable_lines():
-                margin += line.margin
-                margin_signed += line.margin_signed
-                price_subtotal += line.price_subtotal
-            invoice.margin = margin
-            invoice.margin_signed = margin_signed
-            invoice.margin_percent = (
-                price_subtotal and margin_signed / price_subtotal * 100 or 0.0
-            )
-
-
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
